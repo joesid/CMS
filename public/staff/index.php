aa82fe7a1183183dfd2e44c18f846d4e7310a47a
joesid <joesidious@gmail.com>
2023-06-17 19:12:02 +0100

    Chap 2

--- public/staff/index.php
+++ public/staff/index.php
@@ -4,6 +4,12 @@
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
 <div id="content">
+    <div id="main-menu">
+        <h2>Main Menu</h2>
+        <ul>
+            <li><a href="<?php echo url_for('/staff/subjects/index.php'); ?>Subjects</a>
+        </ul>
+    </div>
 
 </div>
 
