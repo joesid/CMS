0c7c22bb6772c4045a792e8c766407b77311c520
joesid <joesidious@gmail.com>
2023-07-18 07:35:11 +0100

    Page Redirection

--- private/functions.php
+++ private/functions.php
@@ -31,4 +31,9 @@ function error_500() {
     header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
     exit();
 }
+
+function redirect_to($location) {
+    header("Location: " . $location);
+    exit;
+}
 ?>
