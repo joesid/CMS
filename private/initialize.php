c29f6fc8d22aaf8ed3740be3214730cf2a5622e8
joesid <joesidious@gmail.com>
2023-07-19 01:25:51 +0100

    implement Output Buffering

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,5 @@
 <?php
+ob_start(); //out put buffering is turned on
 
 //Assign file paths to PHP constants
 // __FILE__ returns the current path to this file
@@ -8,7 +9,13 @@ define("PROJECT_PATH", dirname(PRIVATE_PATH));
 define("PUBLIC_PATH", PROJECT_PATH . '/public');
 define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
-
+// Assign the root URL to a PHP constant
+// * Do not need to include the domain
+// * Use the same document as webserver
+// * Can set a Hardcoded value:
+// define("WWW_ROOT", '/CMS/public');
+//define("WWW_ROOT", '');
+//Can dynamically find everything in URL up to "/public"
 
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
