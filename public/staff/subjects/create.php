d65d36f91e3a8d2e0752dbbc75a8f8e5517043e4
joesid <joesidious@gmail.com>
2023-07-20 04:35:00 +0100

    Detect Form Submission & redirect

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -1,15 +1,21 @@
 <?php 
 
-// handles form values sent by new.php
+require_once('../../../private/initialize.php'); 
 
-$menu_name = $_POST['menu_name'] ?? '';
-$position = $_POST['position'] ?? '';
-$visible = $_POST['visible'] ?? '';
+if(is_post_request()){
+    // handles form values sent by new.php
 
-echo "Form parameters<br />";
-echo "Menu name: " . $menu_name . "<br />";
-echo "Position: " . $position . "<br />";
-echo "Visible: " . $visible . "<br />";
+    $menu_name = $_POST['menu_name'] ?? '';
+    $position = $_POST['position'] ?? '';
+    $visible = $_POST['visible'] ?? '';
 
+    echo "Form parameters<br />";
+    echo "Menu name: " . $menu_name . "<br />";
+    echo "Position: " . $position . "<br />";
+    echo "Visible: " . $visible . "<br />";
+}
+ else {
+    redirect_to(url_for('/staff/subjects/new.php'));
+ }
 
 ?>
