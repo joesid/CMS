0597521c27cd2713672d5b4ab5b59291e803de8e
joesid <joesidious@gmail.com>
2023-09-16 23:38:20 +0100

    Add Form public/staff/new.php - Menu Name

--- public/staff/pages/new.php
+++ public/staff/pages/new.php
@@ -21,3 +21,24 @@ if(is_post_request()) {
 
 <?php $page_title = 'Create Page'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+    <a class="back-link" href="<?php echo url_for('/staff/pages/index.php'); ?>"&laquo; Back to List</a>
+
+    <div class="page new">
+        <h1>Create Page</h1>
+
+        <form action="<?php echo url_for('/staff/pages/new.php');?> method="post">
+        <dl>
+            <dt>Menu Name</dt>
+            <dd><input type="text" name="menu_name" value="<?php echo h($menu_name); ?>" />
+            </dd>
+        </dl>
+        <div id="operations">
+            <input type="submit" value="Create Page" />
+        </div>
+    </form>
+    </div>
+</div>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
